f0cb1945c57692d9f28375d0829c6ff7c28adfa5
rahul-soshte <rahul.soshte47@gmail.com>
2019-09-29 20:01:14 +0530

    Another milestone

---
+++ webpack.config.prod.js
@@ -0,0 +1,215 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const BUILD_DIR = path.resolve(__dirname, 'dist')
+const APP_DIR = path.resolve(__dirname, 'src')
+
+// Html Auto generator
+const HtmlPlugin = new HtmlWebpackPlugin({
+  title: 'Bramble | Token ecosystem for Reward based system',
+  template: require('html-webpack-template'),
+  appMountId: 'app',
+  favicon: 'src/favicon.ico',
+  baseHref: '/',
+  inject: false,
+  mobile: true,
+  scripts: ['https://www.googletagmanager.com/gtag/js?id=UA-126698710-1'],
+  meta: [
+    { charset: 'utf-8' },
+    {
+      name: 'description',
+      content: 'Decentralizing the Proof of Life in the Blockchain'
+    },
+    {
+      name: 'keywords',
+      content:
+        'Karbon14, ICO, Crowdsale, Blockchain, Smart Contracts, Proof of Life, Ethereum, ERC20, ERC-20'
+    },
+    {
+      property: 'og:description',
+      content: 'Token ecosystem for reward based systems'
+    },
+    {
+      property: 'canonical',
+      content: 'https://karbon14.org/'
+    },
+    {
+      property: 'og:image',
+      content: 'https://karbon14.org/preview.jpg'
+    },
+    {
+      property: 'og:title',
+      content: 'Bramble'
+    },
+    {
+      property: 'og:type',
+      content: 'website'
+    },
+    {
+      property: 'og:url',
+      content: 'https://karbon14.org/'
+    },
+    {
+      property: 'og:site_name',
+      content: 'Bramble'
+    },
+    {
+      name: 'google-site-verification',
+      content: '-8U4YlewY12zEvNP-i_1Pgw9DsSa-9mJTwLUBF5TkNI'
+    }
+  ],
+  headHtmlSnippet: `
+    <script type="application/ld+json">
+      {
+        "@context": "http://schema.org",
+        "@type": "Organization",
+        "name": "Karbon14",
+        "url": "https://karbon14.org",
+        "logo": "https://karbon14.org/logo.png",
+        "sameAs": [
+          "https://www.facebook.com/k14project",
+          "https://twitter.com/k14project",
+          "https://github.com/karbon14",
+          "https://medium.com/@karbon14"
+        ]
+      }
+    </script>
+  `
+})
+
+const getPlugins = argv => {
+  let plugins = [
+    HtmlPlugin,
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(argv.mode),
+      'process.env.API_URL': JSON.stringify(
+        process.env.API_URL || 'http://localhost:80/v1'
+      ),
+      'process.env.CROWDSALE_URL': JSON.stringify(
+        process.env.CROWDSALE_URL || 'https://crowdsale.karbon14.org'
+      ),
+      'process.env.AIRDROP_URL': JSON.stringify(
+        process.env.AIRDROP_URL || 'https://airdrop.karbon14.org'
+      ),
+      'process.env.NETWORK': JSON.stringify(process.env.NETWORK || '1')
+    })
+  ]
+
+  if (argv.mode === 'production') {
+    plugins = [
+      ...plugins,
+      new MiniCssExtractPlugin({
+        filename: '[name].css',
+        chunkFilename: '[id].css'
+      }),
+      new CopyWebpackPlugin([
+        {
+          from: './_redirects',
+          to: './_redirects',
+          toType: 'file'
+        },
+        {
+          from: './_headers',
+          to: './_headers',
+          toType: 'file'
+        },
+        {
+          from: './src/Assets/logo-token.png',
+          to: './logo-token.png',
+          toType: 'file'
+        },
+        {
+          from: './src/Assets/logo-token-200.png',
+          to: './logo-token-200.png',
+          toType: 'file'
+        }
+      ])
+    ]
+  }
+
+  return plugins
+}
+
+module.exports = (env, argv) => ({
+  // Don't directly expose sourcemaps on production
+  devtool: argv.mode === 'production' ? false : 'eval-source-map',
+  // webpack-dev-server configurations
+  devServer: {
+    inline: true,
+    contentBase: BUILD_DIR,
+    historyApiFallback: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers':
+        'Origin, X-Requested-With, Content-Type, Accept',
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:80',
+        pathRewrite: { '^/api': '' }
+      }
+    }
+  },
+  // Entry of the project, use babel polyfill.
+  // Can be configured to split into diferent files.
+  entry: {
+    bundle: [
+      'whatwg-fetch',
+      'babel-polyfill',
+      'url-search-params-polyfill',
+      'moment',
+      'moment-timezone',
+      `${APP_DIR}/index.js`
+    ]
+  },
+  output: {
+    path: BUILD_DIR,
+    publicPath: '/',
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js'
+  },
+  module: {
+    // Set up loaders to process your files
+    rules: [
+      {
+        test: /\.(jpe?g|png|gif|svg|woff2?|ttf|eot)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: '[name].[ext]' }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        include: [APP_DIR],
+        use: [{ loader: 'babel-loader' }]
+      },
+      {
+        test: /\.css/,
+        // use: [{ loader: 'to-string-loader' }, { loader: 'css-loader' }]
+        use: [
+          { loader: 'to-string-loader' },
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: 'css-loader' }
+        ]
+      },
+      {
+        test: /\.scss/,
+        use: [{ loader: 'babel-loader' }, { loader: 'styled-jsx-css-loader' }]
+      }
+    ]
+  },
+  plugins: getPlugins(argv),
+  resolve: {
+    alias: {
+      react: require.resolve('react')
+    },
+    // Allow us to use peerDependencies on library packages
+    modules: ['node_modules', path.join(__dirname, 'node_modules')]
+  }
+})
